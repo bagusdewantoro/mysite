15d220c6da671abd47c45367ee11e1d0707d197b
bagusdewantoro <bagusdewantoro86@gmail.com>
2020-10-24 21:26:27 +0700

    04 - templates, static

--- blog/models.py
+++ blog/models.py
@@ -40,7 +40,7 @@ class Post(models.Model):
     published = PublishedManager()   # Our custom manager.
 
     def get_absolute_url(self):
-        return reverse('blog : post_detail',
+        return reverse('blog:post_detail',
                         args = [self.publish.year,
                                 self.publish.month,
                                 self.publish.day,
--- blog/views.py
+++ blog/views.py
@@ -15,4 +15,4 @@ def post_detail(request, year, month, day, post):
                                     publish__day = day)
     return render(request,
                 'blog/post/detail.html',
-                {}'post' : post})
+                {'post' : post})
